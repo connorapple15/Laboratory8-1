91a90bd749f8eb6467431e09b4b2b9ebf1eb81b3
Connor Apple <c-apple@onu.edu>
2024-10-31 08:45:53 -0400

    Completed countCharacter function

--- Laboratory8-1/Source.cpp
+++ Laboratory8-1/Source.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <string>
 using namespace std;
+#include <iostream>
+#include <string>
+
+int countCharacter(const string& str);
+
+int main() {
+    string test1 = "Hello, World!";
+    string test2 = "Laboratory eight";
+
+    cout << "Number of characters in test1: " << countCharacter(test1) << endl;
+    cout << "Number of characters in test2: " << countCharacter(test2) << endl;
+
+    return 0;
+}
 
 int countCharacter(const string& str) {
     return str.length();
-
-    int main() {
+}
